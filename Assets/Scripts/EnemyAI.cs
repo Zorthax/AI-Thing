679a2ea02a2ab5858a608cdea2c8f86af9b6628e
Adam <ad.schmidt7@gmail.com>
2018-09-12 17:51:34 +0930

    Unity Project
    Everything

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour {
+
+    States currentState;
+    
+    enum States
+    {
+        Patrol,
+        Chase,
+        Wait
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+        
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		switch (currentState)
+        {
+            case States.Patrol:
+                Patrol();
+                break;
+            case States.Chase:
+                Chase();
+                break;
+            case States.Wait:
+                Wait();
+                break;
+        }
+	}
+
+    void ChangeToPatrol()
+    {
+
+    }
+
+    void Patrol()
+    {
+
+    }
+
+    void ChnageToChase()
+    {
+
+    }
+
+    void Chase()
+    {
+
+    }
+
+    void ChangeToWait()
+    {
+
+    }
+
+    void Wait()
+    {
+
+    }
+
+
+    /*
+    
+    public Transform[] patrolPoints;
+    public Transform player;
+    public float waitTime;
+
+    NavMeshAgent navAgent;
+    int currentPoint = 0;
+    float waitTimer;
+
+    navAgent = GetComponent<NavMeshAgent>();
+        ChangeToPatrol();
+
+    void ChangeToPatrol()
+    {
+        navAgent.SetDestination(patrolPoints[currentPoint].position);
+        navAgent.isStopped = false;
+        currentState = States.Patrol;
+    }
+
+    void Patrol()
+    {
+
+        if (Vector3.Distance(transform.position, patrolPoints[currentPoint].position) < 1)
+        {
+            currentPoint++;
+            if (currentPoint >= patrolPoints.Length)
+                currentPoint = 0;
+            navAgent.SetDestination(patrolPoints[currentPoint].position);
+        }
+
+        if (Vector3.Distance(player.position, transform.position) < 10)
+        {
+            ChangeToChase();
+        }
+
+    }
+
+    void ChangeToChase()
+    {
+        navAgent.isStopped = false;
+        currentState = States.Chase;
+    }
+
+    void Chase()
+    {
+        navAgent.isStopped = false;
+        navAgent.SetDestination(player.position);
+
+        if (Vector3.Distance(player.position, transform.position) > 30)
+        {
+            ChangeToWait();
+        }
+    }
+
+    void ChangeToWait()
+    {
+        currentState = States.Wait;
+        waitTimer = waitTime;
+    }
+
+    void Wait()
+    {
+        waitTimer -= Time.deltaTime;
+        navAgent.isStopped = true;
+
+        if (waitTimer <= 0)
+        {
+            ChangeToPatrol();
+        }
+    }
+    */
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float _walkSpeed;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        //Input
+        float horizontal = Input.GetAxis("Horizontal") * _walkSpeed * Time.deltaTime;
+        float vertical = Input.GetAxis("Vertical") * _walkSpeed * Time.deltaTime * 500;
+
+        //Movement
+        transform.position += new Vector3(horizontal, 0, vertical);
+	}
+}
